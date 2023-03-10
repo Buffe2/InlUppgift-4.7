d9831036bc353f5b8e57e0548c596ca869a0d625
GustavJohansson420 <gujo04002@utb.vaxjo.se>
2023-03-10 09:02:08 +0100

    Add project files.

---
+++ ConsoleApp37/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace InlUppgift_4._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett meddelande och välj sedan hur många steg ifrån vänsterkanten den ska vara.");
+            string med = Console.ReadLine();
+            int avs = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < med.Length; i++)
+            {
+                for (int j = 0; j < avs; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine(med[i]);
+
+            }
+        }
+    }
+}
